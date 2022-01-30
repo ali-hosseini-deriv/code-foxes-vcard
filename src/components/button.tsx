648a8c95f7b85ff4f1001326a914c7db5fa1daf7
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2022-01-30 18:19:35 +0330

    feat: improve button style

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,18 +1,30 @@
 import { Button as MuiButton } from "@mui/material";
 
 import { styled } from "@mui/styles";
-import { MouseEventHandler } from "react";
-
-const StyledButton = styled(MuiButton)({});
+import React, { MouseEventHandler } from "react";
+const StyledButton = styled(MuiButton)(
+   {
+    backgroundColor: (props)=> props.variant === "contained" ? '#ff444f' : "transparent",
+       borderColor: "#ff444f",
+       borderWidth:  "2px",
+       borderRadius: "6px",
+       color: (props)=> props.variant === "contained" ? "white" : "#d43e47",
+       "&:hover":{
+            backgroundColor:"#d43e47",
+            color: "white",
+            borderColor: "#ff444f",
+        },
+});
 
 type Props = {
   text: string;
-  onclick: MouseEventHandler<HTMLButtonElement>
+  onclick: MouseEventHandler<HTMLButtonElement>;
+  variant?: "text" | "outlined" | "contained";
 };
 
-const Button: React.FC<Props> = ({ text = "", onclick }) => {
+const Button: React.FC<Props> = ({ text = "", onclick, variant="contained" }) => {
   return (
-    <StyledButton variant="contained" onClick={onclick}>
+    <StyledButton variant={variant} onClick={onclick}>
       {text}
     </StyledButton>
   );
