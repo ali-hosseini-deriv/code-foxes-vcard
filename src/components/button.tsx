d07cf90afea89335d92a6b87fb07ebad7f24168b
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2022-02-01 18:49:58 +0330

    feat/ improve button component style

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,33 +1,34 @@
-import { Button as MuiButton } from "@mui/material";
+import {Button as MuiButton} from "@mui/material";
 
-import { styled } from "@mui/styles";
-import React, { MouseEventHandler } from "react";
+import {styled} from "@mui/styles";
+import React, {MouseEventHandler} from "react";
+
 const StyledButton = styled(MuiButton)(
-   {
-    backgroundColor: (props)=> props.variant === "contained" ? '#ff444f' : "transparent",
-       borderColor: "#ff444f",
-       borderWidth:  "2px",
-       borderRadius: "6px",
-       color: (props)=> props.variant === "contained" ? "white" : "#d43e47",
-       "&:hover":{
-            backgroundColor:"#d43e47",
+    {
+        backgroundColor: (props) => props.variant === "contained" ? '#ff444f' : "transparent",
+        borderColor: "#ff444f",
+        borderWidth: "2px",
+        borderRadius: "6px",
+        color: (props) => props.variant === "contained" ? "white" : "#d43e47",
+        "&:hover": {
+            backgroundColor: '#ff444f !important',
             color: "white",
             borderColor: "#ff444f",
         },
-});
+    });
 
 type Props = {
-  text: string;
-  onclick: MouseEventHandler<HTMLButtonElement>;
-  variant?: "text" | "outlined" | "contained";
+    text: string;
+    onclick: MouseEventHandler<HTMLButtonElement>;
+    variant?: "text" | "outlined" | "contained";
 };
 
-const Button: React.FC<Props> = ({ text = "", onclick, variant="contained" }) => {
-  return (
-    <StyledButton variant={variant} onClick={onclick}>
-      {text}
-    </StyledButton>
-  );
+const Button: React.FC<Props> = ({text = "", onclick, variant = "contained"}) => {
+    return (
+        <StyledButton variant={variant} onClick={onclick}>
+            {text}
+        </StyledButton>
+    );
 };
 
 export default Button;
