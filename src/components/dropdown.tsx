4170f524e0be68b66440a3a046580562fe8c3a05
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2022-02-02 18:02:01 +0330

    feat/ improve drop down component

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -1,31 +1,64 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import Select from "@mui/material/Select";
-import { styled } from "@mui/styles";
+import {makeStyles} from "@mui/styles";
 import MenuItem from "@mui/material/MenuItem";
+import { InputLabel} from '@mui/material';
 
-const StyledDropDown = styled(Select)({});
-
+const useStyles = makeStyles(() => ({
+    formControl:{
+        padding: "4px 0 5px",
+        position:"relative",
+        display:"inline-block",
+        width:"100%",
+        paddingRight: '10px !important',
+    },
+    label:{
+        fontWeight:'bold !important',
+        fontSize:"13px !important",
+        marginBottom:'5px',
+        color:'black !important',
+    },
+    select:{
+        '& <div':{
+            minHeight:"44px !important",
+        },
+        width:"100%",
+        backgroundColor:'#f0f3f7',
+        minHeight:"44px !important",
+    }
+}))
 type Props = {
-  value: string;
-  label: string;
-  items: any;
+    value: string;
+    name: string;
+    label: string;
+    items: string[];
+    onChange: (arg: any) => void
 };
 
-type Item = {
-  value: number;
-  text: string;
-};
 
-const DropDown: React.FC<Props> = ({ value, label, items }) => {
-  return (
-    <StyledDropDown value={value} label={label}>
-      {items.map((item: React.PropsWithChildren<Item>) => {
-        console.log("item.value: ", item.value);
-        console.log("item.text: ", item.text);
-        return <MenuItem value={item.value}>{item.text}</MenuItem>;
-      })}
-    </StyledDropDown>
-  );
+const DropDown: React.FC<Props> = ({name, value, label, items, onChange}) => {
+    const styles= useStyles()
+    return (
+
+        <div className={styles.formControl} >
+            <InputLabel className={styles.label} id="country" >{label}</InputLabel>
+
+            <Select
+                className={styles.select}
+                name={name}
+                value={value}
+                variant="standard"
+                onChange={onChange}
+                displayEmpty={true}
+
+            >
+                {items.map((item: string) => {
+                    return <MenuItem value={item}>{item}</MenuItem>;
+                })}
+            </Select>
+        </div>
+
+    );
 };
 
 export default DropDown;
