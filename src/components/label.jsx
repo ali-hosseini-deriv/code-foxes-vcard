10d70ff397ec639b01e5d71935f9885e99566341
sara-fs <sara.khosropour@firstsource.tech>
2022-02-06 12:49:47 +0330

    Reverted stles

---
+++ src/components/label.jsx
@@ -0,0 +1,40 @@
+import {makeStyles} from '@mui/styles';
+import React from 'react'
+import Typography from "@mui/material/Typography";
+
+const useStyles = makeStyles(() => ({
+    container: {
+        borderBottom: "2px #ebe9e9 solid",
+        paddingLeft: "30px !important",
+        marginTop: "15px"
+    },
+    typography: {
+        fontWeight: 'bold',
+        marginTop: "10px",
+        display: 'flex',
+        marginBottom: "-2px !important",
+        borderBottom: "2px #ff444f solid",
+        width: "fit-content"
+        // display:'inline-block'
+    },
+}))
+
+type Props = {
+    variant: any;
+    text: string;
+    color?: string;
+
+}
+
+
+const Label: React.FC<Props> = ({text = '', variant = 'h1', color = 'black'}) => {
+    const styles = useStyles()
+    return (
+        <div className={styles.container}>
+
+            <Typography className={styles.typography} variant={variant} color={color}>{text}</Typography>
+        </div>
+    )};
+
+
+export default Label;
