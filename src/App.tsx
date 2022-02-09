dcf1f5eee289a522e3adf124cc164615e675234d
Ali(Ako) Hosseini <ali.hosseini@firstsource.tech>
2022-02-09 14:06:52 +0330

    fix/ add header to all of pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import QrCodeForm from "./qr-code-form";
 import { Stack } from "@mui/material";
 import Header from "./components/header/header";
 import { makeStyles } from "@mui/styles";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import VisitCard from './visit-card';
 
 const useStyles = makeStyles(() => ({
   center: {
@@ -24,7 +26,21 @@ export default function App() {
       <Stack>
         <Header />
         <div className={styles.center}>
-          <QrCodeForm />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<QrCodeForm />} />
+              <Route path="visit-card" element={<VisitCard />}>
+              </Route>
+              <Route
+                  path="*"
+                  element={
+                    <div style={{ padding: "1rem" }}>
+                      <p>404 Not found...!</p>
+                    </div>
+                  }
+              />
+            </Routes>
+          </BrowserRouter>
         </div>
       </Stack>
     </Container>
